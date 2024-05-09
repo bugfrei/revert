ba8d09ea95420dd8b4cf6c7047a6f440057a2aee
Carsten Schlegel <71234951+bugfrei@users.noreply.github.com>
2024-05-10 01:06:14 +0200

    Add Data-Output in Hello World function

--- t.js
+++ t.js
@@ -1,8 +1,10 @@
 console.log("Demo App");
 
 // Kunde möchte eine Funktion, die Hello World ausgibt
+// Nun soll die Funktion noch Daten ausgeben
 function hello() {
     console.log("Hello World!");
+    console.log(daten);
 }
 
 hello();
